689553c2ae539f5225d8a00b5b5bac0cd6a5d2f6
DareAnanas <nylo2005@gmail.com>
2023-08-12 11:37:44 +0300

    Challenge 3 upgrade. - added second version of challenge 3 - added container fluid with responsize padding to second version - set font-size in rem - react component container now has fluid property

--- src/Pages/Challenge3/Challenge3.jsx
+++ src/Pages/Challenge3/Challenge3.jsx
@@ -1,4 +1,5 @@
 import Challenge3Css from './styles/Challenge3.module.css';
+import Hr from '../../Components/Hr/Hr';
 
 function Challenge3() {
     return (
@@ -12,24 +13,35 @@ function Challenge3() {
                     <Button>I want to learn</Button>
                 </Container>
             </BGC_Container>
+            <Hr subHr={true} />
+            <BGC_Container fluid={true}>
+                <Container fluid={true}>
+                    <IntroContent>
+                        <h2>Responsive layouts don't have to be a struggle</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </IntroContent>
+                    <Button>I want to learn</Button>
+                </Container>
+            </BGC_Container>
         </>
     );
 }
 
-function BGC_Container({children, color}) {
+function BGC_Container({children, color, fluid}) {
     return (
         <div className={
-            Challenge3Css['bgc-container']} 
-            style={color?{'backgroundColor': color}:{}}
+            fluid ? Challenge3Css['bgc-container-fluid'] : Challenge3Css['bgc-container']} 
+            style={color ? {'backgroundColor': color} : {}}
         >
             {children}
         </div>
     );
 }
 
-function Container({children}) {
+function Container({children, fluid}) {
     return (
-        <div className={Challenge3Css.container}>
+        <div className={
+            fluid ? Challenge3Css['container-fluid'] : Challenge3Css.container}>
             {children}
         </div>
     );
