9ad881dd72cbf4f29f02e50f28063e4346a53769
DareAnanas <nylo2005@gmail.com>
2023-08-13 10:03:13 +0300

    Completed flexbox challenge 1. - added custom bool properties $primary ... $centered to bgc_container - added custom string property $gap to row - some stupid design cognitive distortions in CSS

--- src/Pages/Challenge4/Challenge4.jsx
+++ src/Pages/Challenge4/Challenge4.jsx
@@ -2,13 +2,18 @@ import { styled } from "styled-components";
 import { Link } from "react-router-dom";
 
 const BGC_Container = styled.div`
-    background: #23424A;
-    color: white;
-    padding: 4em 0;
+    background: ${props => {
+        if (props.$primary) return '#23424A';
+        if (props.$secondary) return '#136C72';
+        return 'white';
+    }};
+    color: ${props => (props.$primary || props.$secondary) ? 'white' : '#3A3A3A'};
+    padding: 8em 0;
+    text-align: ${props => props.$centered ? 'center' : 'initial'};
 `;
 
 const Container = styled.div`
-    max-width: 1000px;
+    max-width: 1200px;
     width: 80%;
     margin: auto;
 `;
@@ -31,12 +36,13 @@ const P = styled.p`
     font-size: 1.3rem;
     font-weight: 400;
     line-height: 31px;
-    text-align: left;
+    margin: 0;
 `;
 
 const StyledLink = styled(Link)`
     display: inline-block;
     padding: 0.5em 2em;
+    margin-top: 2em;
     text-transform: uppercase;
     text-decoration: none;
     color: black;
@@ -55,16 +61,62 @@ const StyledLink = styled(Link)`
     }
 `;
 
+const Row = styled.div`
+    display: flex;
+    gap: ${props => props.$gap || '20px'};
+`;
+
+const Col = styled.div``;
+
+const H3 = styled.h3`
+    color: ${props => props.$primary ? '#87629A' : 'inherit'};
+    font-family: Roboto;
+    font-size: 2.25rem;
+    margin-top: 0;
+`;
+
 function Challenge4() {
     return (
         <>
-            <BGC_Container>
+            <BGC_Container $primary>
                 <Container>
                     <IntroContent>
                         <H2>Responsive layouts don't have to be a struggle</H2>
                         <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</P>
                         <StyledLink to="/challenge3">I want to learn</StyledLink>
                     </IntroContent>
+                </Container>
+            </BGC_Container>
+            <BGC_Container $centered>
+                <Container>
+                    <Row>
+                        <Col>
+                            <H3 $primary>Cheap</H3>
+                            <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</P>
+                        </Col>
+                        <Col>
+                            <H3 $primary>Quick</H3>
+                            <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</P>
+                        </Col>
+                        <Col>
+                            <H3 $primary>Quality</H3>
+                            <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</P>
+                        </Col>
+                    </Row>
+                </Container>
+            </BGC_Container>
+            <BGC_Container $secondary $centered>
+                <Container>
+                    <Row $gap='60px'>
+                        <Col>
+                            <H3>Cheap</H3>
+                            <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</P>
+                        </Col>
+                        <Col>
+                            <H3>Quick</H3>
+                            <P>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</P>
+                        </Col>
+                    </Row>
                 </Container>
             </BGC_Container>
         </>
