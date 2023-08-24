628a6db5b166618de3f7936424496d9faaee6a69
DareAnanas <nylo2005@gmail.com>
2023-08-24 13:12:37 +0300

    Babel styled components plugin

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react({
+        babel: {
+        // Your plugins run before any built-in transform (eg: Fast Refresh)
+        plugins: [
+            ["babel-plugin-styled-components", {fileName: false}]
+        ],
+        // Use .babelrc files
+        babelrc: true,
+        // Use babel.config.js files
+        configFile: true,
+        }
+    })],
 });
